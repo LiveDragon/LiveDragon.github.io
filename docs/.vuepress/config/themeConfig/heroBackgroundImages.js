e9031801474f9cb69acf56110ee66cc067f85f04
longyunfei <807094491@qq.com>
2021-08-18 18:26:52 +0800

    更新博文

--- docs/.vuepress/config/themeConfig/heroBackgroundImages.js
+++ docs/.vuepress/config/themeConfig/heroBackgroundImages.js
@@ -8,5 +8,6 @@ module.exports = [
   'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4e44d3ce43eb4d848fba4dbed087cb3a~tplv-k3u1fbpfcp-watermark.image',
   'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/8c6814ab1b804733a5be15ce1a57a98b~tplv-k3u1fbpfcp-watermark.image',
   'https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c0ece32d775247e9b04f37e19f05d4c5~tplv-k3u1fbpfcp-watermark.image',
-  'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c04d5c871462480aa6b2b59f1f419807~tplv-k3u1fbpfcp-watermark.image'
+  'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c04d5c871462480aa6b2b59f1f419807~tplv-k3u1fbpfcp-watermark.image',
+  'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/848bbca550564f6c93a83d42835762dd~tplv-k3u1fbpfcp-watermark.image'
 ];
